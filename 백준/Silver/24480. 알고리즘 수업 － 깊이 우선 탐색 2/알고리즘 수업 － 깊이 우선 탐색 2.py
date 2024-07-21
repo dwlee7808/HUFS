f26a70486d7eb76991f6dde83bea7450d87d906b
dwlee7808 <155355676+dwlee7808@users.noreply.github.com>
2024-07-21 17:22:47 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 608 ms, Memory: 67996 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -1,31 +1,31 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6) #재귀 최대 깊이 설정. 런타임 에러 해결
 
-n, m, r = map(int,input().split())
-link = [[] for _ in range(n+1)]
+n, m, r = map(int,input().split()) #n, m, r 입력
+link = [[] for _ in range(n+1)] #인접 리스트로 그래프 생성
 
 
 for _ in range (m):
     a, b = map(int, input().split())
-    link[a].append(b)
-    link[b].append(a)
+    link[a].append(b) 
+    link[b].append(a) #입력받은 a, b 연결. 인접 리스트에 추가
     
-visited = [0]*(n+1)
-visited[r] = 1
-count = 1
+visited = [0]*(n+1) #visited 리스트로 방문 순서 저장
+visited[r] = 1 #
+count = 1 #방문 순서
 
 
 def dfs(v):
     global count
-    link[v].sort(reverse = True)
+    link[v].sort(reverse = True) #내림차순으로 정렬
     for i in link[v]:
-        if visited[i] == 0:
-            count += 1
+        if visited[i] == 0: #방문하지 않은 정점 
+            count += 1 
             visited[i] = count
             dfs(i)
             
-dfs(r)
+dfs(r) # 시작 정점 r
 
 for i in range (1, n+1):
     print(visited[i])
